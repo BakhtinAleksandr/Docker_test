4522ea0b1fe172c789ad0613939b7685432ad4d6
BakhtinAleksandr <1sasha12001@mail.ru>
2022-12-03 15:44:33 +0300

    Docker_test
    version1

---
+++ version1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace version1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
